62043be445915cced42f17389e4b3a75c8fdef8b
Tran Dinh Duc <153018979+ductd2k3@users.noreply.github.com>
2024-10-14 17:45:49 +0700

    Update DBContext.java

--- src/java/DAO/DBContext.java
+++ src/java/DAO/DBContext.java
@@ -8,15 +8,15 @@ public class DBContext {
 
     // Configuration for MySQL connection
     private final String serverName = "localhost";
-    private final String dbName = "drinkingorder";
+    private final String dbName = "DrinkingOrder";
     private final String portNumber = "3306"; // Default MySQL port
     private final String userID = "root"; // Replace with your MySQL username
-    private final String password = "root"; // Replace with your MySQL password
+    private final String password = "123456"; // Replace with your MySQL password
 
     // Method to establish a database connection
     public Connection getConnection() {
         Connection conn = null;
-        String url = "jdbc:mysql://" + serverName + ":" + portNumber + "/" + dbName + "?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+        String url = "jdbc:mysql://" + serverName + ":" + portNumber + "/" + dbName + "?useSSL=false&serverTimezone=UTC";
 
         try {
             // Load MySQL JDBC driver
@@ -27,7 +27,7 @@ public class DBContext {
             System.err.println("MySQL JDBC Driver not found.");
             e.printStackTrace();
         } catch (SQLException e) {
-            System.err.println("Failed to establish a connection to the MySQL database."  + e.getMessage());
+            System.err.println("Failed to establish a connection to the MySQL database.");
             e.printStackTrace();
         }
 
